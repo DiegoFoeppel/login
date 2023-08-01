890b6a3b8169d97acaad383e6d3d4d93772a0a52
DiegoFoeppel <73116936+DiegoFoeppel@users.noreply.github.com>
2023-08-01 19:08:39 -0300

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,13 @@
+function showMenu() {
+  console.log("it works");
+
+  //document.querySelector(".menu").style.display = "none";
+  console.log(document.getElementsByClassName("menu").style);
+  /*
+  if (document.querySelector(".menu").style.display == "") {
+    console.log("hello");
+  }*/
+}
+
+function showProfile() {}
+function logout() {}
